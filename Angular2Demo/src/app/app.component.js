d35269fb277fc69a8c71c073af02ef790c44e133
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-11-04 18:46:32 -0600

    both interpolation and property binding sanitizes malicious content.

--- Angular2Demo/src/app/app.component.js
+++ Angular2Demo/src/app/app.component.js
@@ -12,11 +12,13 @@ var AppComponent = /** @class */ (function () {
         this.pageHeader = 'Employee Details';
         this.firstName = 'Tom';
         this.imagePath = 'https://www.premierleague.com/resources/ver/i/elements/premier-league-logo-header.svg';
+        this.isDisabled = false;
+        this.badHtml = 'Hello <script>alert("Hacked");</script> World';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<div>\n                    <h1>{{pageHeader}}</h1>\n                    <h2>{{'first Name: '+firstName}}</h2>\n                    <img src='{{imagePath}}'/>\n                    <my-employee></my-employee>\n               </div>",
+            template: "<div>\n                    <h1>{{pageHeader}}</h1>\n                    <h2>{{'first Name: '+firstName}}</h2>\n                    <img src='{{imagePath}}'/>\n                    <!--using property binding: notice that I have used square bracket for image emement source property and quote for component class property.-->\n                    <img [src]='imagePath'/>\n                    <my-employee></my-employee>\n                    <br/>{{badHtml}} <br/>\n                  \n                    <button [disabled]='isDisabled'>Click me</button>\n\n               </div>",
         })
     ], AppComponent);
     return AppComponent;
--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+    selector: 'my-app',
     template: `<div>
                     <h1>{{pageHeader}}</h1>
                     <h2>{{'first Name: '+firstName}}</h2>
                     <img src='{{imagePath}}'/>
+                    <!--using property binding: notice that I have used square bracket for image emement source property and quote for component class property.-->
+                    <img [src]='imagePath'/>
                     <my-employee></my-employee>
+                    <br/>{{badHtml}} <br/>
+                  
+                    <button [disabled]='isDisabled'>Click me</button>
+
                </div>`,
 })
-export class AppComponent
-{
+export class AppComponent {
     pageHeader = 'Employee Details';
     firstName: string = 'Tom';
     imagePath: string = 'https://www.premierleague.com/resources/ver/i/elements/premier-league-logo-header.svg';
+    isDisabled: boolean = false;
+    badHtml: string = 'Hello <script>alert("Hacked");</script> World';
 }
+
