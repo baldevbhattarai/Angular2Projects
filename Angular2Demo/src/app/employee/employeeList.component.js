a9b151a787b60f2114ac400dba789f0cb1332ad8
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-11-10 14:45:44 -0600

    Pipes, are used to transform data before display, tate, currency, lowercase, uppercase etc are inbuilt available pipes.

--- Angular2Demo/src/app/employee/employeeList.component.js
+++ Angular2Demo/src/app/employee/employeeList.component.js
@@ -15,7 +15,7 @@ var EmployeeListComponent = /** @class */ (function () {
         this.employees = [
             {
                 code: 'emp101', name: 'Tom', gender: 'Male',
-                annualSalary: 5500, dateOfBirth: '25/6/1988'
+                annualSalary: 5500, dateOfBirth: '5/26/1988'
             },
             {
                 code: 'emp102', name: 'Alex', gender: 'Male',
@@ -27,7 +27,7 @@ var EmployeeListComponent = /** @class */ (function () {
             },
             {
                 code: 'emp104', name: 'Mary', gender: 'Female',
-                annualSalary: 6500.826, dateOfBirth: '14/10/1980'
+                annualSalary: 6500.826, dateOfBirth: '4/14/1980'
             },
         ];
     }
@@ -51,7 +51,7 @@ var EmployeeListComponent = /** @class */ (function () {
             },
             {
                 code: 'emp105', name: 'Nancy', gender: 'Female',
-                annualSalary: 6700.826, dateOfBirth: '15/12/1982'
+                annualSalary: 6700.826, dateOfBirth: '5/12/1982'
             },
         ];
     };
--- Angular2Demo/src/app/employee/employeeList.component.ts
+++ Angular2Demo/src/app/employee/employeeList.component.ts
@@ -13,7 +13,7 @@ export class EmployeeListComponent {
         this.employees = [
             {
                 code: 'emp101', name: 'Tom', gender: 'Male',
-                annualSalary: 5500, dateOfBirth: '25/6/1988'
+                annualSalary: 5500, dateOfBirth: '5/26/1988'
             },
             {
                 code: 'emp102', name: 'Alex', gender: 'Male',
@@ -25,7 +25,7 @@ export class EmployeeListComponent {
             },
             {
                 code: 'emp104', name: 'Mary', gender: 'Female',
-                annualSalary: 6500.826, dateOfBirth: '14/10/1980'
+                annualSalary: 6500.826, dateOfBirth: '4/14/1980'
             },
         ];
     }
@@ -50,7 +50,7 @@ export class EmployeeListComponent {
             },
             {
                 code: 'emp105', name: 'Nancy', gender: 'Female',
-                annualSalary: 6700.826, dateOfBirth: '15/12/1982'
+                annualSalary: 6700.826, dateOfBirth: '5/12/1982'
             },
         ];
     }
