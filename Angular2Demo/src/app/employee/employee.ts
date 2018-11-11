93d9d05c930e777c649f81304b35dc188913cfa2
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-11-11 14:16:26 -0600

    Interface in angular.

---
+++ Angular2Demo/src/app/employee/employee.ts
@@ -0,0 +1,11 @@
+﻿export interface IEmployee {
+    code: string;
+    name: string;
+    gender: string;
+    annualSalary: number;
+    dateOfBirth: string;
+    // To make a property optional use a ?
+    // A class that implements this interface need
+    // not provide implementation for this property
+    //department?: string;
+}
